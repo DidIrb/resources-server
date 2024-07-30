8bd1869f08450064cbd41aa5cb8e9af27765c1e8
DidIrb <irbayedubadida@gmail.com>
2024-07-30 22:36:20 +0300

    formating sitemap response

--- src/controllers/sitemap.controller.ts
+++ src/controllers/sitemap.controller.ts
@@ -4,37 +4,35 @@ import { Resources } from '../types/data.types';
 import { site } from '../config/auth.config';
 
 const generateXML = (req: Request, res: Response) => {
-    try {
-        const data = getDataFromJson();
-        const sitemapXml = generateSitemapXml(data);
-        res.header('Content-Type', 'application/xml');
-        res.header('Content-Disposition', 'attachment; filename="sitemap.xml"');
-        res.send(sitemapXml);
-    } catch (error: any) {
-        res.status(500).json({ error: error.message });
-    }
+  try {
+    const data = getDataFromJson();
+    const sitemapXml = generateSitemapXml(data);
+    res.header('Content-Type', 'application/xml');
+    res.header('Content-Disposition', 'attachment; filename="sitemap.xml"');
+    res.send(sitemapXml);
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 const generateSitemapXml = (data: any) => {
-    const sitemapXml = `
-      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-        <url>
-            <loc>${site}</loc>
-            <lastmod>2024-07-30</lastmod>
-        </url>
-        <url>
-            <loc>${site}/home</loc>
-            <lastmod>2024-07-30</lastmod>
-        </url>
-        ${data.map((item: Resources) => `
-          <url>
-            <loc>${site}/resource/${item.title}</loc>
-            <lastmod>${item.updatedAt}</lastmod>
-          </url>
-        `).join('\n')}
-      </urlset>
-    `;
-    return sitemapXml;
+  const sitemapXml = `
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+        <loc>${site}</loc>
+        <lastmod>2024-07-30</lastmod>
+    </url>
+    <url>
+        <loc>${site}/home</loc>
+        <lastmod>2024-07-30</lastmod>
+    </url>${data.map((item: Resources) => `
+    <url>
+        <loc>${site}/resource/${item.title}</loc>
+        <lastmod>${item.updatedAt}</lastmod>
+    </url>`).join('\n')}
+  </urlset>
+`;
+  return sitemapXml;
 };
 
 export default { generateXML }
